6f783c6f7da24b676323655b9efacfcb1c027ad9
JosueEsc7676 <JosueEscobar000@hotmail.com>
2025-09-06 18:09:17 -0600

    EjercisioJSON

---
+++ EjercisioJSON/src/main/java/sv/edu/esfe/Encuesta.java
@@ -0,0 +1,10 @@
+package sv.edu.esfe;
+
+import java.util.List;
+
+public class Encuesta {
+    public int Id;
+    public String Titulo;
+    public String Fecha;
+    public List<Preguntas> Preguntas;
+}
