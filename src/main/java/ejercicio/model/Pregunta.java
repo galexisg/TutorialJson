52e4e0717eee4ba4f7c06921766f9f9c0acb04f8
jontj7 <jont_martinez@outlook.com>
2025-09-11 21:23:24 -0600

    solucion de los ejercicios de practicas

---
+++ src/main/java/ejercicio/model/Pregunta.java
@@ -0,0 +1,12 @@
+package ejercicio.model;
+
+
+import java.util.List;
+
+
+public class Pregunta {
+    public int id;
+    public String texto;
+    public boolean esObligatoria ;
+    public List<Opcion> opciones;
+}
---
+++ src/main/java/sv/edu/esfe/model/Cliente.java
@@ -0,0 +1,7 @@
+package sv.edu.esfe.model;
+
+public class Cliente {
+    public int id;
+    public String nombre;
+    public String correo;
+}
